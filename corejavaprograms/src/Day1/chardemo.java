21ec23371ef5848ea651a27ff194e614096e529b
HP <HP@LAPTOP-TVK4UDTJ>
2025-09-23 21:15:21 +0530

    day 1 completed

---
+++ corejavaprograms/src/Day1/chardemo.java
@@ -0,0 +1,24 @@
+package Day1;
+
+public class chardemo {
+
+	public static void main(String[] args) {
+		
+			char chr = 'a';
+			System.out.println(chr);
+
+			char chr1 = 65;
+			System.out.println(chr1);
+
+			char variable1 = '\u00A7';
+			System.out.println(variable1);
+
+			char variable2 = '\u20AC';
+			System.out.println(variable2);
+
+			int a = 'A';
+			System.out.println(a);
+
+	}
+
+}
---
+++ corejavaprograms/src/Day1/identifier.java
@@ -0,0 +1,15 @@
+package Day1;
+
+public class identifier {
+
+	public static void main(String[] args) {
+		
+		int $num = 14; 
+		System.out.println("value of the number variable is : "+ $num);
+	
+		String studentName="Gaja"; 
+		System.out.println("value is : "+ studentName);
+
+	}
+
+}
