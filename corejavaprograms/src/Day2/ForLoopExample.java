0e74d09599530d02d775347c805e52d90450d053
HP <HP@LAPTOP-TVK4UDTJ>
2025-09-27 12:54:26 +0530

    Day 2 is completed

---
+++ corejavaprograms/src/Day2/ForLoopExample.java
@@ -0,0 +1,14 @@
+package Day2;
+
+public class ForLoopExample {
+
+	public static void main(String[] args) {
+		for(int i = 0; i<=1000 ; i+=5)   
+		{	
+			System.out.print("Value of i:");
+			System.out.println(i);
+		}
+
+	}
+
+}
