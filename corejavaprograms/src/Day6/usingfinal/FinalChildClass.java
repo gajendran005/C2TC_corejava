0fa5774fc2736f050a0c3d54ea558aa06a7773b3
HP <HP@LAPTOP-TVK4UDTJ>
2025-10-02 20:20:02 +0530

    Day 6 completed

---
+++ corejavaprograms/src/Day6/usingfinal/FinalChildClass.java
@@ -0,0 +1,7 @@
+package Day6.usingfinal;
+
+class FinalChildClass {
+	   FinalChildClass() {
+	      throw new Error("Unresolved compilation problem: \n\tThe type FinalChildClass cannot subclass the final class FinalClass\n");
+	   }
+	}
