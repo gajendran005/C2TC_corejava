9c017fd8845336c2fc33dad094382ffca3a4d952
HP <HP@LAPTOP-TVK4UDTJ>
2025-10-05 19:21:31 +0530

    DAY  7 IS COMPLETED

--- corejavaprograms/src/Day6/usingfinal/FinalChildClass.java
+++
@@ -1,7 +0,0 @@
-package Day6.usingfinal;
-
-class FinalChildClass {
-	   FinalChildClass() {
-	      throw new Error("Unresolved compilation problem: \n\tThe type FinalChildClass cannot subclass the final class FinalClass\n");
-	   }
-	}
--- corejavaprograms/src/Day6/usingfinal/FinalClassDemo.java
+++ corejavaprograms/src/Day6/usingfinal/FinalClassDemo.java
@@ -10,10 +10,12 @@ final class FinalClass {
 class FinalChildClass extends FinalClass {
 }
 
+
 public class FinalClassDemo {
 	public static void main(String[] args) {
 		// Create the object of final class
 		FinalClass f1 = new FinalClass(); // Call show() method using object reference variable ab.
 		f1.show();
 	}
+
 }
